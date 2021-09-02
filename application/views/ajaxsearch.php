c6fb97b08839fcbca267e4c9af548d193599cb8a
purnima13 <62002282+purnima13@users.noreply.github.com>
2021-09-03 00:03:43 +0530

    Update ajaxsearch.php

--- application/views/ajaxsearch.php
+++ application/views/ajaxsearch.php
@@ -1,34 +1,85 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Live Data Search in Codeigniter using Ajax JQuery</title>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
-	</head>
-	<body style="background-color: lightblue;">
-		<div class="container">
-			<br />
-			<br />
-			<br />
-			<p align="center" style="font-size: 55px;">Live Data Search of Stock in Codeigniter</p><br />
-			<div class="form-group">
-				<div class="input-group">
-					<span class="input-group-addon">Search</span>
-					<input type="text" name="search_text" id="search_text" placeholder="Search Stock Details" class="form-control" />
-				</div>
-			</div>
-			<br />
-			<div id="result"></div>
-		</div>
-		<div style="clear:both"></div>
-		<br />
-		<br />
-		<br />
-		<br />
-	</body>
-</html>
+  <head>
+    <title>Awesome Search Box</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
+<style type="text/css">
+	
+    body,html{
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    background: #e74c3c !important;
+    }
+
+    .searchbar{
+    margin-bottom: 50%;
+    //margin-top: 50%;
+    height: 60px;
+    background-color: #353b48;
+    border-radius: 30px;
+    padding: 10px;
+    }
+
+    .search_input{
+    color: white;
+    border: 0;
+    outline: 0;
+    background: none;
+    width: 0;
+    caret-color:transparent;
+    line-height: 40px;
+    transition: width 0.4s linear;
+    }
+
+    .searchbar:hover > .search_input{
+    padding: 0 10px;
+    width: 450px;
+    caret-color:red;
+    transition: width 0.4s linear;
+    }
+
+    .searchbar:hover > .search_icon{
+    background: white;
+    color: #e74c3c;
+    }
+
+    .search_icon{
+    height: 40px;
+    width: 40px;
+    float: right;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    color:white;
+    text-decoration:none;
+    }
+
+    #result{
+    	margin-top: -50%;
+    }
+</style>
+  </head>
+  <body>
+    <div class="container h-100" style="margin-bottom=50%;">
+      <div class="d-flex justify-content-center h-100">
+        <div class="searchbar">
+          <input class="search_input" type="text" name="search_text" id="search_text" placeholder="Search the stock details here...">
+          <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
+        </div>
+      </div>
+	<div id="result" style="margin-bottom=40%;"></div>
+	</div>
+	<div style="clear:both"></div>
+  </body>
+</html>
 
 <script>
 $(document).ready(function(){
