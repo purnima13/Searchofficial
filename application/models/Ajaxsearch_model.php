1ec19a739c5267e16da265c94fbc057d79a4574e
purnima13 <62002282+purnima13@users.noreply.github.com>
2021-09-03 07:14:02 +0530

    Update Ajaxsearch_model.php

--- application/models/Ajaxsearch_model.php
+++ application/models/Ajaxsearch_model.php
@@ -1,18 +1,11 @@
 <?php
 class Ajaxsearch_model extends CI_Model
 {
-	function fetch_data($query)
-	{
-		$this->db->select("*");
-		$this->db->from("tbl_stock");
-		if($query != '')
-		{
-			$this->db->like('SName',$query);
-		    $this->db->or_like('Current_Market_Price', $query);
-
-		}
-		$this->db->order_by('SNo','asc');
-		return $this->db->get();
-	}
+	function search_title($title){
+        $this->db->like('SName', $title , 'both');
+        $this->db->order_by('SNo', 'ASC');
+        $this->db->limit(5);
+        return $this->db->get('tbl_stock')->result();
+    }
 }
 ?>
