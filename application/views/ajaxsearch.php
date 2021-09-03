fd0f4f51274d66a9cd9b71a20985d36290a325d7
purnima13 <62002282+purnima13@users.noreply.github.com>
2021-09-03 15:44:35 +0530

    Update ajaxsearch.php

--- application/views/ajaxsearch.php
+++ application/views/ajaxsearch.php
@@ -7,12 +7,12 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 <style type="text/css">
 	
-	body,html{
+body,html{
     height: 100%;
     width: 100%;
     margin: 0;
@@ -67,28 +67,83 @@
     #result{
     	margin-top: -50%;
     }
+	
 </style>
+	  
   </head>
   <body>
     <div class="container h-100" style="margin-bottom=50%;">
       <div class="d-flex justify-content-center h-100">
         <div class="searchbar">
-          <input class="search_input" type="text" id="title" placeholder="Search the stock details here...">
+          <input class="typeahead search_input" type="text" name="search_text" id="search_text" placeholder="Search the stock details here...">
           <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
         </div>
       </div>
-			<div id="result" style="margin-bottom=40%;"></div>
-		</div>
-		<div style="clear:both"></div>
+	<div id="result" style="margin-bottom=40%;display: none;"></div>
+	</div>
+	<div style="clear:both"></div>
   </body>
 </html>
 
-
-
 <script>
+
+
+var BASE_URL = "<?php echo base_url(); ?>";
+
 $(document).ready(function(){
-            $( "#title" ).autocomplete({
-              source: "<?php echo site_url('ajaxsearch/get_autocomplete/?');?>"
-            });
+
+	load_data();
+
+   $('#search_text').keyup(function() { 
+ 
+   $( "#result" ).show(8000); 
+ }); 
+
+
+  $( "#search_text" ).autocomplete({
+ 
+        source: function(request, response) {
+            $.ajax({
+            url: BASE_URL + "ajaxsearch/search",
+            data: {
+                    term : request.term
+             },
+            dataType: "json",
+            success: function(data){
+               var resp = $.map(data,function(obj){
+                    return obj.SName;
+               }); 
+ 
+               response(resp);
+            }
         });
+    },
+    minLength: 1
+ });
+
+
+	function load_data(query)
+	{
+		$.ajax({
+			url:"<?php echo base_url(); ?>ajaxsearch/fetchdata",
+			method:"POST",
+			data:{query:query},
+			success:function(data){
+				$('#result').html(data);
+			}
+		})
+	}
+
+	$('#search_text').keyup(function(){
+		var search = $(this).val();
+		if(search != '')
+		{
+			load_data(search);
+		}
+		else
+		{
+			load_data();
+		}
+	});
+});
 </script>
